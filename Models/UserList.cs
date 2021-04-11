122a9f12ee692757dd754f79eb3b7d1c65eb820d
jan <admin@example.com>
2021-04-11 17:00:26 +0800

    feat:dapper plus merge single

--- Models/UserList.cs
+++ Models/UserList.cs
@@ -5,6 +5,7 @@ namespace Models
     public class User
     {
         public int ID { get; set; }
+        public string Name {get;set;}
         public string Account { get; set; }
         public string Password { get; set; }
         public long Mail { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Models;
 using MySqlConnector;
 using Z.Dapper.Plus;
@@ -17,16 +18,25 @@ namespace termainalc
             var elapsedMs = watch.ElapsedMilliseconds;
 
             DapperPlusManager.Entity<User>().Table("User").Identity(x => x.ID);
-            DapperPlusManager.Entity<UserGroup>().Table("UserGroup").Identity(x => x.Id).
-            Ignore(x => x.User).AfterAction((kind,x) => {
-                if(kind == DapperPlusActionKind.Insert || kind == DapperPlusActionKind.Merge){
-                    x.User.UserGroupId = x.Id;
-                }
-            });
+            // DapperPlusManager.Entity<UserGroup>().Table("UserGroup").Identity(x => x.Id).
+            // Ignore(x => x.User).AfterAction((kind,x) => {
+            //     if(kind == DapperPlusActionKind.Insert || kind == DapperPlusActionKind.Merge){
+            //         x.User.UserGroupId = x.Id;
+            //     }
+            // });
 
-            var user = new User { Account = "user1" };
-            var userGroup = new UserGroup{Name = "group1",User = user};
-            _conn.BulkInsert(userGroup).ThenBulkInsert(x => x.User);
+            var user = new List<User>{
+                new User{
+                    Name = "UserA",
+                    UserGroupId = 1
+                }
+            };
+
+            _conn.BulkMerge(user);
+
+            // var user = new User { Account = "user1" };
+            // var userGroup = new UserGroup{Name = "group1",User = user};
+            // _conn.BulkInsert(userGroup).ThenBulkInsert(x => x.User);
             Console.WriteLine(elapsedMs);
             watch.Stop();
         }
